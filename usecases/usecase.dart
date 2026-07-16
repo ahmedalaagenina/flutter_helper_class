ee6199930c2012d52bad827c14127648afaed0e7
Ahmed Alaa <ahmedalaagenina@gmail.com>
2026-07-16 10:01:23 +0300

    feat: introduce BaseUseCase with Result type wrapper and update NoParams class

--- usecases/usecase.dart
+++ usecases/usecase.dart
@@ -1,6 +1,20 @@
+import '../networking/api_call_handler.dart';
 
 abstract class UseCase<T, Params> {
   Future<T> call(Params params);
 }
 
 class NoParams {}
+
+/// Abstract base class for all use cases
+/// [T]: Return type of the use case (usually Entity)
+/// [Params]: Parameters required for the use case
+abstract class BaseUseCase<T, Params> {
+  /// Execute the use case
+  Future<Result<T>> call(Params params);
+}
+
+/// Use case with no parameters
+class NoParams {
+  const NoParams();
+}
